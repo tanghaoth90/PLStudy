08107cdfae1e21038e50200db69a3d4e83a9d827
tanghao <tanghaoth90@gmail.com>
2017-02-06 21:44:25 +0800

    add results: raw language statistics of repos

--- FetchRepoInfo/fetch_repo_list.rb
+++ FetchRepoInfo/fetch_repo_list.rb
@@ -3,24 +3,26 @@
 require 'octokit'
 require 'pp'
 
-#Octokit.auto_paginate = true
+Octokit.auto_paginate = true
+
+fout = File.open("logs/langs.log", "w")
 
 repos = Octokit.search_repos 'stars:>1000', \
 	:sort => 'stars', :order => 'desc', \
 	:per_page => 100  
 	
-puts 'Total Repos = ' + repos.items.length.to_s + "\n"
-
-puts 'x-ratelimit-remaining after searching = ' \
-	+ Octokit.last_response.headers['x-ratelimit-remaining']
+puts 'Total Repos = ' + repos.items.length.to_s
+puts 'XRR = ' + Octokit.last_response.headers['x-ratelimit-remaining']
 
 repos.items.each_with_index do |repo,index|
-	puts '#' + index.to_s + ' ' + repo.full_name # + ' ' + repo.git_url
-	puts repo.languages_url
+	#puts '#' + index.to_s + ' ' + repo.full_name # + ' ' + repo.git_url
+	#puts repo.languages_url
 	langs_response = repo.rels[:languages].get
-	puts 'x-ratelimit-remaining after getting languages = ' \
-		+ langs_response.headers['x-ratelimit-remaining']
+	puts repo.full_name
 	pp langs_response.data
-	break
+	puts 'XRR = ' + langs_response.headers['x-ratelimit-remaining']
+	
+	fout.puts repo.full_name
+	PP.pp langs_response.data, fout
 end
 
