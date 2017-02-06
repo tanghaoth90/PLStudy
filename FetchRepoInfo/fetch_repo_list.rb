7a6465b85cb212d7a79c267938639476a3aa2c86
tanghao <tanghaoth90@gmail.com>
2017-02-06 19:06:45 +0800

    fetch languages for the top repos

--- FetchRepoInfo/fetch_repo_list.rb
+++ FetchRepoInfo/fetch_repo_list.rb
@@ -1,26 +1,26 @@
 #!/bin/bash ruby
 
 require 'octokit'
+require 'pp'
 
-#client = Octokit::Client.new(:access_token => ENV['GITACCESS'])
+#Octokit.auto_paginate = true
 
-#client = Octokit.readme 'tanghaoth90/PLStudy' \
-#	, :accept => 'application/vnd.github.html'
-
-puts Octokit.access_token
-Octokit.auto_paginate = true
-
-repos = Octokit.search_repositories 'stars:>1000', \
+repos = Octokit.search_repos 'stars:>1000', \
 	:sort => 'stars', :order => 'desc', \
-	:headers => { 'X-GitHub-OTP' => Octokit.access_token }
+	:per_page => 100  
 	
 puts 'Total Repos = ' + repos.items.length.to_s + "\n"
 
+puts 'x-ratelimit-remaining after searching = ' \
+	+ Octokit.last_response.headers['x-ratelimit-remaining']
+
 repos.items.each_with_index do |repo,index|
-	puts '#' + index.to_s
-	puts repo.full_name + ' ' + repo.git_url
-	puts repo.languages_url + "\n"
+	puts '#' + index.to_s + ' ' + repo.full_name # + ' ' + repo.git_url
+	puts repo.languages_url
+	langs_response = repo.rels[:languages].get
+	puts 'x-ratelimit-remaining after getting languages = ' \
+		+ langs_response.headers['x-ratelimit-remaining']
+	pp langs_response.data
+	break
 end
-
-
 
