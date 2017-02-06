801b19837cc3aa11dbbe68040acbb77874ebacad
tanghao <tanghaoth90@gmail.com>
2017-02-06 23:56:02 +0800

    select languages

--- FetchRepoInfo/stat_langs.rb
+++ FetchRepoInfo/stat_langs.rb
@@ -1,9 +1,21 @@
+require 'pp'
+
+sel_langs = [:C, :'C++', :'C#', :'Objective-C', :Go, \
+	:Java, :CoffeeScript, :JavaScript, :TypeScript, \
+	:Ruby, :Php, :Python, :Perl, :Clojure, :Erlang, \
+	:Haskell, :Scala]
+
 fin = open 'results/langs.log', 'r'
 
 while repo_name = fin.gets
-	langs = ""
+	repo_name.delete!("\n")
+	langs_str = ""
 	while (line = fin.gets) != "\n"
-		langs += line.delete!("\n")
+		langs_str += line.delete("\n")
 	end
-	puts repo_name + " " + langs 
+	langs = eval langs_str
+	langs.select!{|key,value| sel_langs.include? key}
+	puts repo_name
+	pp langs
 end
+
