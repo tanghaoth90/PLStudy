33361eb7235bec348b6045473afaf103cdc1744d
tanghao <tanghaoth90@gmail.com>
2017-03-01 15:47:24 +0800

    get readmes

--- FetchRepoInfo/fetch_desc_readme.rb
+++ FetchRepoInfo/fetch_desc_readme.rb
@@ -6,26 +6,37 @@ $sel_langs = [:C, :'C++', :'C#', :'Objective-C', :Go, \
 	:Ruby, :Php, :Python, :Perl, :Clojure, :Erlang, \
 	:Haskell, :Scala]
 
-def parse_get_description()
-	fout = File.open 'results/desc.log', 'w'
-	open 'results/langs.log', 'r' do |fin|
-		while r_name = fin.gets
-			r_name.delete!("\n")
-			r_langs_str = ""
-			while (line = fin.gets) != "\n"
-				r_langs_str += line.delete("\n")
-			end
-			r_langs = eval r_langs_str
-			r_langs.select!{ |k,v| $sel_langs.include? k }
-			if r_langs.size > 0
-				repo = Octokit.repository r_name
-				puts r_name
-				puts repo.description
-				fout.puts r_name
-				fout.puts repo.description
-			end
+flagdesc = false
+flagreadme = true
+fout = File.open 'results/desc.log', 'w'
+fout_readme = File.open 'results/readmes/readmes.log', 'w'
+fin = File.open 'results/langs.log', 'r'
+i = 0
+while r_name = fin.gets
+	r_name.delete!("\n")
+	r_langs_str = ""
+	while (line = fin.gets) != "\n"
+		r_langs_str += line.delete("\n")
+	end
+	r_langs = eval r_langs_str
+	r_langs.select!{ |k,v| $sel_langs.include? k }
+	if r_langs.size > 0
+		if flagdesc 
+			repo = Octokit.repository r_name
+			puts r_name
+			puts repo.description
+			fout.puts r_name
+			fout.puts repo.description
+		end
+		if flagreadme
+			rdm = Octokit.readme r_name, :accept => 'application/vnd.github.raw'
+			i += 1
+			puts r_name + ' ' + i.to_s
+			fout_readme.puts r_name + " " + i.to_s
+			fout_newreadme = File.open 'results/readmes/' + i.to_s + '.readme', 'w'
+			fout_newreadme.puts rdm
+			fout_newreadme.close
 		end
 	end
 end
 
-parse_get_description
